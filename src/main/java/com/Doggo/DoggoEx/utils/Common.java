69d0e7613674ce96804fddb2b41eb93daee4fdd1
lastofyears21 <lastofyears21@gmail.com>
2023-12-10 18:57:09 +0900

    23/12/10 스웨거 등록 및 , 프론트쪽과 연동 완료

--- src/main/java/com/Doggo/DoggoEx/utils/Common.java
+++ src/main/java/com/Doggo/DoggoEx/utils/Common.java
@@ -1,7 +1,7 @@
-//package com.Doggo.DoggoEx.utils;
-//
-//public class Common {
-//    // CORS 설정
-//    public final static String CORS_ORIGIN = "http://localhost:3000";
-//}
-//
+package com.Doggo.DoggoEx.utils;
+
+public class Common {
+    // CORS 설정
+    public final static String CORS_ORIGIN = "http://localhost:3000";
+}
+
